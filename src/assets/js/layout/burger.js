d253c8671a967ed8261cc79722a7537e95480673
Nika <n.nishnianidze@limecs.net>
2021-09-24 14:10:43 +0400

    fix 4 tasks

--- src/assets/js/layout/burger.js
+++ src/assets/js/layout/burger.js
@@ -1,8 +1,10 @@
 export const burgerFunction = () => {
     const burger = document.querySelector('.header__burger')
     const header__nav = document.querySelector('.header__nav')
+    const header__content = document.querySelector('.header__content')
     burger.addEventListener('click', () => {
         burger.classList.toggle('active')
         header__nav.classList.toggle('toggled')
+        header__content.classList.toggle('white')
     })
 }

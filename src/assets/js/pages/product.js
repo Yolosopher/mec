a0eb204e8b7c122159225ae85cce9f1c0e2dec94
Nika Nishnianidze <nika.nishnianidze.1@iliauni.edu.ge>
2021-09-25 23:40:40 +0400

    f

--- src/assets/js/pages/product.js
+++ src/assets/js/pages/product.js
@@ -25,3 +25,179 @@ const imageSwiper = new Swiper('.productpage__slider__container', {
     },
 })
 
+
+// NICE SELECTS
+
+;[...document.querySelectorAll('.ns')].forEach(ns => {
+	// toggler
+    ns.addEventListener('click', () => {
+        ns.classList.toggle('toggled')
+        // if (!ns.classList.contains('toggled')) {
+        // }
+    })
+    
+    // inner functions
+    let select = ns.querySelector('select')
+	let ns_ul = ns.querySelector('ul')
+
+	;[...select.querySelectorAll('option')].forEach(option => {
+		let newLi = document.createElement('li')
+		newLi.innerText = option.innerText
+		newLi.dataset.value = option.value
+		if (option.selected) newLi.classList.add('active')
+		ns_ul.appendChild(newLi)
+        newLi.addEventListener('click', () => {
+            select.value = option.value
+            ns_ul.querySelector('li.active').classList.remove('active')
+            newLi.classList.add('active')
+            // ns.classList.remove('toggled')
+        }, true)
+	})
+
+    ns_ul.style.setProperty('--height', ns_ul.scrollHeight + 'px')
+})
+
+
+// DETAILS TOGGLER
+;[...document.querySelectorAll('.productpage__details__each')].forEach(detaileach => {
+    detaileach.style.setProperty('--height', detaileach.scrollHeight + 'px')
+    let heading = detaileach.querySelector('h2')
+    heading.addEventListener('click', () => {
+        detaileach.classList.toggle('toggled')
+    })
+})
+
+// ALSO INTERESTING - slider
+const alsoInterestingSwiper = new Swiper('.homecategory__slider__container', {
+    slidesPerView: 1,
+    breakpoints: {
+        700: {
+            slidesPerView: 3
+        },
+        1025: {
+            slidesPerView: 4
+        }
+    },
+    navigation: {
+        nextEl: '.homecategory__slider__nav__right',
+        prevEl: '.homecategory__slider__nav__left',
+        disabledClass: 'disabled'
+    },
+})
+
+// MODAL FUNCTIONALITY
+// caller
+const productpage__info__link = document.querySelector('.productpage__info__link')
+
+const ordermodal = document.querySelector('.ordermodal')
+const ordermodal__bg = document.querySelector('.ordermodal__bg')
+const ordermodal__X = document.querySelector('.ordermodal__X')
+
+const orderModalToggler = (show = true) => {
+    if (show) {
+        ordermodal__bg.classList.add('toggled')
+        ordermodal.classList.add('toggled')
+    } else {
+        ordermodal__bg.classList.remove('toggled')
+        ordermodal.classList.remove('toggled')    
+    }
+}
+
+
+productpage__info__link.addEventListener('click', () => {
+    orderModalToggler()
+})
+ordermodal__bg.addEventListener('click', () => {
+    orderModalToggler(false)
+})
+ordermodal__X.addEventListener('click', () => {
+    orderModalToggler(false)
+})
+
+// VALIDATIONS
+const nameChecker = (el, onInput = false, atleast = 5) => {
+	let val = el.value
+	if (onInput) {
+		if (val === '') {
+			el.classList.add('invalid')
+			el.classList.remove('invalid-shown')
+		} else if (val.length < atleast) {
+			el.classList.add('invalid')
+			el.classList.add('invalid-shown')
+		} else {
+			el.classList.remove('invalid')
+			el.classList.remove('invalid-shown')
+		}
+	} else {
+		if (val === '') {
+			el.parentElement.classList.add('invalid')
+			el.parentElement.classList.remove('invalid-shown')
+		} else if (val.length < atleast) {
+			el.parentElement.classList.add('invalid')
+			el.parentElement.classList.add('invalid-shown')
+		} else {
+			el.parentElement.classList.remove('invalid')
+			el.parentElement.classList.remove('invalid-shown')
+		}
+	}
+}
+
+const emailChecker = (el, onInput = false) => {
+	let val = el.value
+	let ifEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(val)
+	if (onInput) {
+		if (val === '') {
+			el.classList.add('invalid')
+			el.classList.remove('invalid-shown')
+		} else if (!ifEmail) {
+			el.classList.add('invalid')
+			el.classList.add('invalid-shown')
+		} else {
+			el.classList.remove('invalid')
+			el.classList.remove('invalid-shown')
+		}
+	} else {
+		if (val === '') {
+			el.parentElement.classList.add('invalid')
+			el.parentElement.classList.remove('invalid-shown')
+		} else if (!ifEmail) {
+			el.parentElement.classList.add('invalid')
+			el.parentElement.classList.add('invalid-shown')
+		} else {
+			el.parentElement.classList.remove('invalid')
+			el.parentElement.classList.remove('invalid-shown')
+		}
+	}
+}
+
+const orderfirstname = document.getElementById('orderfirstname')
+const orderlastname = document.getElementById('orderlastname')
+const orderemail = document.getElementById('orderemail')
+const ordertel = document.getElementById('ordertel')
+
+orderfirstname.addEventListener('change', () => {
+	nameChecker(orderfirstname, true, 2)
+})
+orderlastname.addEventListener('change', () => {
+	nameChecker(orderlastname, true, 2)
+})
+orderemail.addEventListener('change', () => {
+	emailChecker(orderemail, true)
+})
+ordertel.addEventListener('change', () => {
+	nameChecker(ordertel, true)
+})
+
+ordermodal.addEventListener('submit', e => {
+	nameChecker(orderfirstname, true, 2)
+    nameChecker(orderlastname, true, 2)
+    emailChecker(orderemail, true)
+    nameChecker(ordertel, true)
+    
+	if (ordermodal.querySelectorAll('.invalid')[0]) {
+		e.preventDefault()
+		ordermodal.querySelectorAll('.invalid').forEach((each) => {
+			each.classList.add('invalid-shown')
+		})
+	}
+})

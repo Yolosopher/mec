c8510ff2b11243081ae0912670b4b54732f5d585
YoloNitro <nika.nishnianidze.1@iliauni.edu.ge>
2021-11-04 00:43:55 +0400

    designers page

--- src/assets/js/pages/designer.js
+++ src/assets/js/pages/designer.js
@@ -6,3 +6,26 @@ window.addEventListener('load', () => {
 	burgerFunction()
 	headerCategory()
 })
+
+// HERO SWIPER
+import Swiper, { Navigation, Pagination } from 'swiper'
+Swiper.use([Navigation, Pagination])
+
+
+// production - slider
+const alsoInterestingSwiper = new Swiper('.homecategory__slider__container', {
+	slidesPerView: 1,
+	breakpoints: {
+			700: {
+					slidesPerView: 3
+			},
+			1025: {
+					slidesPerView: 4
+			}
+	},
+	navigation: {
+			nextEl: '.homecategory__slider__nav__right',
+			prevEl: '.homecategory__slider__nav__left',
+			disabledClass: 'disabled'
+	},
+})

1201e8a71cf8d6f192c13e7df2ab54e81b2e3841
YoloNitro <nika.nishnianidze.1@iliauni.edu.ge>
2021-11-30 14:42:54 +0400

    hero slider updated

--- src/assets/js/pages/home.js
+++ src/assets/js/pages/home.js
@@ -1,77 +1,87 @@
 // LAYOUTS
-import { burgerFunction } from '../layout/burger';
-import { headerCategory } from '../layout/headerCategory';
+import { burgerFunction } from '../layout/burger'
+import { headerCategory } from '../layout/headerCategory'
 
 window.addEventListener('load', () => {
-    burgerFunction()
-    headerCategory()
+	burgerFunction()
+	headerCategory()
 })
-
 
 // HERO SWIPER
 import Swiper, { Navigation, Pagination } from 'swiper'
 Swiper.use([Navigation, Pagination])
 const heroSwiper = new Swiper('.hero__slider__container', {
 	slidesPerView: 1,
-	spaceBetween: 10,
-    navigation: {
-        nextEl: '.hero__slider__nav__right',
-        prevEl: '.hero__slider__nav__left',
-        disabledClass: 'disabled'
-    },
-    pagination: {
-        el: '.hero__slider__pagination',
-        type: 'bullets',
-        bulletClass: 'hero__slider__pagination__bullet',
-        bulletActiveClass: 'active',
-        clickable: true
-    },
+	spaceBetween: 20,
+	loop: true,
+    speed: 700,
+	// autoplay: {
+    //     disableOnInteraction: false,
+	// 	delay: 1000,
+	// },
+	breakpoints: {
+		1025: {
+			centeredSlides: true,
+			slidesPerView: 'auto',
+			spaceBetween: 20,
+		},
+	},
+	navigation: {
+		nextEl: '.hero__slider__nav__right',
+		prevEl: '.hero__slider__nav__left',
+		disabledClass: 'disabled',
+	},
+	pagination: {
+		el: '.hero__slider__pagination',
+		type: 'bullets',
+		bulletClass: 'hero__slider__pagination__bullet',
+		bulletActiveClass: 'active',
+		clickable: true,
+	},
 })
 
-
 const categorySwiper = new Swiper('.homecategory__slider__container', {
-    slidesPerView: 1.0373,
-    breakpoints: {
-        700: {
-            slidesPerView: 2
-        },
-        1025: {
-            slidesPerView: 4
-        }
-    },
-    navigation: {
-        nextEl: '.homecategory__slider__nav__right',
-        prevEl: '.homecategory__slider__nav__left',
-        disabledClass: 'disabled'
-    },
+	slidesPerView: 1.0373,
+	breakpoints: {
+		700: {
+			slidesPerView: 2,
+		},
+		1025: {
+			slidesPerView: 4,
+		},
+	},
+	navigation: {
+		nextEl: '.homecategory__slider__nav__right',
+		prevEl: '.homecategory__slider__nav__left',
+		disabledClass: 'disabled',
+	},
 })
 
 const weekOfferSwiper = new Swiper('.weekoffer__slider__container', {
-    slidesPerView: 1,
-    navigation: {
-        nextEl: '.weekoffer__slider__nav__right',
-        prevEl: '.weekoffer__slider__nav__left',
-        disabledClass: 'disabled'
-    },
+	slidesPerView: 1,
+	navigation: {
+		nextEl: '.weekoffer__slider__nav__right',
+		prevEl: '.weekoffer__slider__nav__left',
+		disabledClass: 'disabled',
+	},
 })
 
-
 const blogSwiper = new Swiper('.homeblog__slider__container', {
-    slidesPerView: 1,
-    breakpoints: {
-        700: {
-            slidesPerView: 2
-        },
-        1025: {
-            slidesPerView: 3
-        },
-        1920: {
-            slidesPerView: 4
-        }
-    },
-    navigation: {
-        nextEl: '.homeblog__slider__nav__right',
-        prevEl: '.homeblog__slider__nav__left',
-        disabledClass: 'disabled'
-    },
+	slidesPerView: 1,
+	breakpoints: {
+		700: {
+			slidesPerView: 2,
+		},
+		1025: {
+			slidesPerView: 3,
+		},
+		1920: {
+			slidesPerView: 4,
+		},
+	},
+	navigation: {
+		nextEl: '.homeblog__slider__nav__right',
+		prevEl: '.homeblog__slider__nav__left',
+		disabledClass: 'disabled',
+	},
 })
